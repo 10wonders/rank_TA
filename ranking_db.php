dc0205c5f65b368f71a4b6537827fb7f5c1eb908
Lee Kun Woo <dlrjsdn4357@gamil.com>
2016-10-18 14:28:46 +0900

    genre search code edit  // itunes genre english language

--- ranking_db.php
+++ ranking_db.php
@@ -129,13 +129,13 @@ ini_set('max_execution_time', 0);
         curl_close($it_ch);
 
         // 링크추출 
-		preg_match_all("/<h3><a[\s]+[^>]*?href[\s]?=[\s\"\']+"."(.*?)[\"\']+.*?>"."([^<]+|.*?)?<\/a>/", $it_content, $it_link_match); 
+		preg_match_all("/<h3><a\shref=\"(.*?)\?/", $it_content, $it_link_match);
 	    // img 추출 
 	    preg_match_all("/<img\ssrc=\"(.*?)\".*?>/", $it_img_content, $it_img_match);
 	    //이름 추출 
 		preg_match_all("/<h3>+<a.*?>(.*?)<\/a>/",$it_content, $it_match);
 		//장르 추출
-		preg_match_all("/<h4>+<a.*?>(.*?)<\/a>/",$it_content, $it_genre_match);
+		/*preg_match_all("/<h4>+<a.*?>(.*?)<\/a>/",$it_content, $it_genre_match);*/
 
 		$date = date('Y-m-d');
 		$date = date("Y-m-d",strtotime(str_replace('/','-',$date))); //today
@@ -179,11 +179,11 @@ ini_set('max_execution_time', 0);
     		$num =mysqli_num_rows($result);
 
     		if($num){ //똑같은 앱정보 이미 있음
-    			$sql = "UPDATE app_info SET it_id='$id', it_genre='$genre' WHERE name='$name'";
+    			$sql = "UPDATE app_info SET it_id='$id', it_link='$itunes_link' WHERE name='$name'";
     			$res = mysqli_query($connect, $sql);
     		}
     		else{
-    			$result= mysqli_query($connect, "INSERT INTO app_info (it_id, img, name, isFree, it_genre) VALUES('$id', '$img', '$name', '$is_free', '$genre');");
+    			$result= mysqli_query($connect, "INSERT INTO app_info (it_id, img, name, isFree, it_link) VALUES('$id', '$img', '$name', '$is_free','$itunes_link');");
     		}
     		$id_it = "id_it_".$free_paid;
 			//아이튠즈 무료 앱 정보, 순위 DB 저장		    			
@@ -194,6 +194,37 @@ ini_set('max_execution_time', 0);
     	mysqli_close($connect);
 
     }
+
+	function it_genre(){
+		include('db_con.php');
+		$sql = "SELECT it_link FROM app_info WHERE it_genre IS NULL AND it_id IS NOT NULL";
+		$result = mysqli_query($connect, $sql);
+		$num= mysqli_num_rows($result);
+		for($i=0; $i<$num; $i++){
+			$it_link = mysqli_fetch_row($result)[0];
+			$it_url = $it_link."?l=en";
+			$it_ch = curl_init();
+			curl_setopt($it_ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36");
+			curl_setopt($it_ch, CURLOPT_TIMEOUT, 60);
+			curl_setopt($it_ch, CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt($it_ch, CURLOPT_URL, $it_url);
+			//curl_setopt($it_ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($it_ch, CURLOPT_REFERER, $ref_url);
+			curl_setopt($it_ch, CURLOPT_HEADER, TRUE);
+			curl_setopt($it_ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+			curl_setopt($it_ch, CURLOPT_FOLLOWLOCATION, TRUE);
+			curl_setopt($it_ch, CURLOPT_POST, TRUE);
+			curl_setopt($it_ch, CURLOPT_POSTFIELDS, $data);
+			$it_link_content = curl_exec ($it_ch);
+			curl_close($it_ch);
+			preg_match('/<span itemprop="applicationCategory".*?>(.*?)<\/span>/', $it_link_content, $it_genre_match);
+			$it_genre = $it_genre_match[1];
+			$it_genre = preg_replace("/&#38;/","&", $it_genre);
+			$it_genre = preg_replace("/&amp;","&", $it_genre);
+			mysqli_query($connect, "UPDATE app_info SET it_genre='$it_genre' WHERE it_link='$it_link'");
+		}
+    	mysqli_close($connect);
+	}
 
 
 
@@ -222,9 +253,10 @@ ini_set('max_execution_time', 0);
 		    		break;
 		    }
 
-		ps_DB($free_paid, $country, $is_free);
-		it_DB($free_paid, $country, $is_free);
+		//ps_DB($free_paid, $country, $is_free);
+		//it_DB($free_paid, $country, $is_free);
 		}
 	}
 	ps_genre();
+	it_genre();
 ?>
